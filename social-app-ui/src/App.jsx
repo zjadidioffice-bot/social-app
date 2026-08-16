c29387a0c0031d1a5ff5ac4dc4c3886a9f1f828c
ZahraJadidi <zjadidioffice@gmail.com>
2026-08-16 09:33:30 +0330

    message

--- social-app-ui/src/App.jsx
+++ social-app-ui/src/App.jsx
@@ -1,28 +1,68 @@
-import {useEffect,useState} from "react"
+import { useEffect, useState } from "react"
 import Post from "./components/Post"
 function App() {
-  const [posts,setPosts]=useState([]);
-  useEffect(()=>{
+  const [posts, setPosts] = useState([]);
+  const [author, setAuthor] = useState("");
+  const [content, setContent] = useState("");
+
+  const handleSubmit = async () => {
+    try {
+      const response = await fetch("http://localhost:3000/api/posts", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          author,
+          content,
+        }),
+      });
+      const newPost = await response.json();
+      console.log(newPost)
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
     fetch("http://localhost:3000/api/posts")
-    .then((response)=>response.json())
-    .then((data)=>{
-      setPosts(data);
-    });
-  },[]);
+      .then((response) => response.json())
+      .then((data) => {
+        setPosts(data);
+      });
+  }, []);
   return (
     <div>
       <h1>Social App</h1>
+      <h2>create post</h2>
+      <input
+        type="text"
+        placeholder="Author"
+        value={author}
+        onChange={(e) => setAuthor(e.target.value)}
+      />
+      <p>{author}</p>
+      <br /><br />
+      <textarea
+        placeholder="write your post"
+        value={content}
+        onChange={(e) => setContent(e.target.value)}
+      />
+      <p>{content}</p>
 
+      <br /><br />
+      <button onClick={handleSubmit}>create post</button>
       <h2>Posts</h2>
 
       {
-        posts.map((post)=>
+        posts.map((post) =>
         (
-                    <Post
-          key={post._id}
-          author={post.author}
-          content={post.content}
-          likes={post.likes}
+          <Post
+            key={post._id}
+            author={post.author}
+            content={post.content}
+            likes={post.likes}
           />
         )
 
