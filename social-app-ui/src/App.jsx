0796f78a36b281a8cef51392a4f36926ed7f17c2
ZahraJadidi <50663340+zjsemnanuniversity@users.noreply.github.com>
2026-08-22 22:26:18 +0330

    routes

--- social-app-ui/src/App.jsx
+++ social-app-ui/src/App.jsx
@@ -1,5 +1,9 @@
 import { useEffect, useState } from "react"
 import Post from "./components/Post"
+import { Routes,Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import CreatePost from "./pages/CreatePost";
+import SinglePost from "./pages/SinglePost"
 function App() {
   const [posts, setPosts] = useState([]);
   const [author, setAuthor] = useState("");
@@ -83,67 +87,103 @@ function App() {
         setPosts(data);
       });
   }, []);
-  return (
-    <div>
-      <h1>Social App</h1>
-      <h2>create post</h2>
-      <input
-        type="text"
-        placeholder="Author"
-        value={author}
-        onChange={(e) => setAuthor(e.target.value)}
+
+return (
+<>
+<nav>
+  <Link to="/">Home</Link>
+  {"|"}
+  <Link to="/create">craete post</Link>
+</nav>
+
+  <Routes>
+
+    <Route
+      path="/"
+      element={
+      <Home
+      posts={posts}
+      onDelete={handleDelete}
+      onEdit={handleEdit}
       />
-      <p>{author}</p>
-      <br /><br />
-      <textarea
-        placeholder="write your post"
-        value={content}
-        onChange={(e) => setContent(e.target.value)}
-      />
-      <p>{content}</p>
+    }
+    />
 
-      <br /><br />
-      <button onClick={handleSubmit}>create post</button>
-      {message && <p>{message}</p>}
-      <h2>Posts</h2>
-      {
-        editingPost && (
-          <div>
-            <h2>editing post</h2>
-            <textarea
-              value={editingContent}
-              onChange={(e) => setEditingContent(e.target.value)}
-              rows="5"
-              cols="40"
-            />
-            <br />
-            <button onClick={handleSave}>save</button>
-            <button
-              onClick={() => setEditingPost(null)}
-            >
-              cancle
-            </button>
-          </div>
-        )
-      }
-      {
-        posts.map((post) =>
-        (
-          <Post
-            key={post._id}
-            id={post._id}
-            author={post.author}
-            content={post.content}
-            likes={post.likes}
-            onDelete={handleDelete}
-            onEdit={handleEdit}
-          />
-        )
+    <Route
+      path="/create"
+      element={<CreatePost />}
+    />
 
-        )
-      }
+    <Route
+      path="/post/:id"
+      element={<SinglePost />}
+    />
 
-    </div>
+  </Routes>
+</>
+
+
+
+    // <div>
+    //   <h1>Social App</h1>
+    //   <h2>create post</h2>
+    //   <input
+    //     type="text"
+    //     placeholder="Author"
+    //     value={author}
+    //     onChange={(e) => setAuthor(e.target.value)}
+    //   />
+    //   <p>{author}</p>
+    //   <br /><br />
+    //   <textarea
+    //     placeholder="write your post"
+    //     value={content}
+    //     onChange={(e) => setContent(e.target.value)}
+    //   />
+    //   <p>{content}</p>
+
+    //   <br /><br />
+    //   <button onClick={handleSubmit}>create post</button>
+    //   {message && <p>{message}</p>}
+    //   <h2>Posts</h2>
+    //   {
+    //     editingPost && (
+    //       <div>
+    //         <h2>editing post</h2>
+    //         <textarea
+    //           value={editingContent}
+    //           onChange={(e) => setEditingContent(e.target.value)}
+    //           rows="5"
+    //           cols="40"
+    //         />
+    //         <br />
+    //         <button onClick={handleSave}>save</button>
+    //         <button
+    //           onClick={() => setEditingPost(null)}
+    //         >
+    //           cancle
+    //         </button>
+    //       </div>
+    //     )
+    //   }
+    //   {
+    //     posts.map((post) =>
+    //     (
+    //       <Post
+    //         key={post._id}
+    //         id={post._id}
+    //         author={post.author}
+    //         content={post.content}
+    //         likes={post.likes}
+    //         onDelete={handleDelete}
+    //         onEdit={handleEdit}
+    //       />
+    //     )
+
+    //     )
+    //   }
+
+    // </div>
   );
 }
 
--- social-app-ui/src/pages/Home.jsx
+++ social-app-ui/src/pages/Home.jsx
@@ -1,4 +1,30 @@
-function Home(){
-    return <h1>Home Page</h1>
+import Post from "../components/Post"
+function Home({ posts, onDelete, onEdit }) {
+    return
+    (
+        <div>
+            <h1>Home Page</h1>
+            <h2>posts</h2>
+
+            
+       {
+        posts.map((post) =>
+        (
+          <Post
+            key={post._id}
+            id={post._id}
+            author={post.author}
+            content={post.content}
+            likes={post.likes}
+            onDelete={handleDelete}
+            onEdit={handleEdit}
+          />
+        )
+
+        )
+      }
+        </div>
+
+    )
 }
 export default Home;
